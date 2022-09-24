970532f981d11a49e7ea1cfa9a339928a0f2183d
LucasEtica <ribackiti@gmail.com>
2022-09-24 18:21:26 -0300

    Mais sobre Rotas.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   get 'welcome/index'
+  get '/inicio', to: 'welcome#index'
   resources :coins
+
+  root to: "welcome#index"
+
+  # O link abaixo é da documentação das routes.
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
